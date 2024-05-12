4e92c786f646a57e80684009e10ba0a6109afa75
joão pedro silva <130461387+jprime617@users.noreply.github.com>
2024-05-12 19:09:31 -0300

    commit 5

--- Your-Friend-You/src/screens/AboutUs.jsx
+++ Your-Friend-You/src/screens/AboutUs.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import '../style.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Nos from '../components/Nos'
 
 
 function AboutUs() {
   return (
     <main>
         <Header />
-        <Texto />
         <Footer />
     </main>
   )
