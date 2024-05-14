b438b154f35a0dbe1d845d1550dfef19d07bfa28
joão pedro silva <130461387+jprime617@users.noreply.github.com>
2024-05-14 11:02:18 -0300

    commit 9

--- Your-Friend-You/src/components/Navbar.jsx
+++ Your-Friend-You/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
     return(
         <nav className="Navbar">
-            <li><a href="/sobre">Sobre</a></li>
+            <li><a href="">Sobre</a></li>
             <li><a href="">Prototipo</a></li>
             <li><a href="">Trello</a></li>
             <li><a target="blank" href="https://github.com/jprime617/desenvolvimento-de-sistemas.git"><img src="../public/github.png" alt="" /></a></li>
