c8b6528e7c702fbd4e9af67591a58e06073ce288
Lucas Grohe Gomes <lucas_g_gomes@estudante.sesisenai.org.br>
2024-05-14 12:12:47 -0300

    commit massa

--- Your-Friend-You/src/components/Banner.jsx
+++ Your-Friend-You/src/components/Banner.jsx
@@ -1,7 +1,27 @@
 export default function Banner(){
     return(
         <section className="container-banner">
-            <img src="../public/banner.jpg" alt="" />
+        <ul className="slider">
+
+        <li>
+          <input type="radio" id="slide1" name="slide" checked></input>
+          <label for="slide1"></label>
+          <img src="1.jpg" />
+        </li>
+
+        <li>
+          <input type="radio" id="slide2" name="slide"></input>
+          <label for="slide2"></label>
+          <img src="6.jpg" />
+        </li>
+
+        <li>
+          <input type="radio" id="slide3" name="slide"></input>
+          <label for="slide3"></label>
+          <img src="23.jpg" />
+        </li>
+
+        </ul>
         </section>
     )
 }
