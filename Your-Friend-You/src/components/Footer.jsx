e417bb8f50f16ed48fcda115e5c14916a6eed2ce
João Vinicius de Aquino Farias <131161400+ApolloMorningstar@users.noreply.github.com>
2024-05-14 12:24:42 -0300

    commit 14

--- Your-Friend-You/src/components/Footer.jsx
+++ Your-Friend-You/src/components/Footer.jsx
@@ -1,14 +1,15 @@
 export default function Footer(){
-    return(
-            <footer className="footer">
-              <div className="footer-container">
-                <div className="contact-info">
-                  <h2>Entre em Contato</h2>
-                  <p>Email: yourfriendyou@example.com</p>
-                  <p>Telefone: (00) 1234-5678</p>
-                  <p>Empresa: Your Friend You</p>
-                </div>
+  return(
+          <footer className="footer">
+           <div className="footer-left">
+            <h2>@ 2024 - Todos os direitos reservados</h2>
+           </div>
+              <div className="contact-info">
+                <h2><strong> Entre em Contato </strong></h2>
+                <p>Email: yourfriendyou@gmail.com</p>
+                <p>Telefone: +55 (48) 9862-5708 </p>
+                <p>Empresa: Your Friend You</p>
               </div>
-            </footer>
-          )
-        }
+          </footer>
+        )
+      }
