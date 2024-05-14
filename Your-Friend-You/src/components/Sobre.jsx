db165527f540e0399083efc0e1d10675f114dcc4
joão pedro silva <130461387+jprime617@users.noreply.github.com>
2024-05-14 10:46:56 -0300

    commit 8

--- Your-Friend-You/src/components/Sobre.jsx
+++ Your-Friend-You/src/components/Sobre.jsx
@@ -2,12 +2,12 @@ import React from "react"
 
 export default function Sobre() {
     return (
-      <section className="container-sobre">
-        <div>
-          <div className="text-sobre"> {/* Correção aqui: classe correta é text-sobre */}
+      <section className="sec-sobre">
+        <div className="container-sobre">
+          <div className="text-sobre"> 
             <h1>Sobre Nós</h1>
             <p>
-              Somos um time do balaco baco
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
             </p>
           </div>
           <div className="img-sobre">
