f0059abe74d5e8dacecab667d4793ade0ca811c4
Nicoly Silva de Castro <129177560+NicolySilvaDeCastro@users.noreply.github.com>
2024-05-15 14:13:36 -0300

    commit 15

--- Your-Friend-You/src/components/Sobre.jsx
+++ Your-Friend-You/src/components/Sobre.jsx
@@ -22,9 +22,6 @@ menos uma versão beta para a apresentação do trabalho final e disponibilizaç
 público geral.
             </p>
           </div>
-          <div className="img-sobre">
-            <img src="../public/placeholder.webp" alt="" />
-          </div>
         </div>
       </section>
     );
