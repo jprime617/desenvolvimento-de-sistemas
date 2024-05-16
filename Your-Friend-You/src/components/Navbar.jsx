d9f42b3cfac451cb976455cccfa1494e9f23897e
João Vinicius de Aquino Farias <131161400+ApolloMorningstar@users.noreply.github.com>
2024-05-16 09:49:37 -0300

    commit final

--- Your-Friend-You/src/components/Navbar.jsx
+++ Your-Friend-You/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
     return(
         <nav className="Navbar">
-            <li><a href="#sobre-nos">Sobre nós</a></li>
+            <li><a href="#sobre-nos">Sobre</a></li>
             <li><a href="https://www.figma.com/file/dsT9bBbQMpp5aYY94EKPuw/Your-Friend-You-%28High%29?type=design&node-id=0%3A1&mode=design&t=Kp4G3kQgUOe8cf5s-1&authuser=0">Figma</a></li>
             <li><a target="blank" href="https://github.com/jprime617/desenvolvimento-de-sistemas">Github</a></li>
         </nav>
