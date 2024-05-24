179423ee094bca397f49f6f21d6b10dea42da97a
João Vinicius de Aquino Farias <131161400+ApolloMorningstar@users.noreply.github.com>
2024-05-24 18:50:55 -0300

    blog_finalizado

--- Your-Friend-You/src/components/BlogS.jsx
+++ Your-Friend-You/src/components/BlogS.jsx
@@ -1,18 +1,33 @@
 export default function BlogS() {
     return(
-        <section className="container-blog">
-            <div>
-                <img src="../public/placeholder.webp" alt="" />
-                <p>Post01</p>
-            </div>
-            <div>
-                <img src="../public/placeholder.webp" alt="" />
-                <p>Post02</p>
-            </div>
-            <div>
-                <img src="../public/placeholder.webp" alt="" />
-                <p>Post03</p>
-            </div>
-        </section>
+        <>
+          <h1 className="titulo_blog">BLOG</h1>
+        <div className="blog">
+             <section className="Trecho1_blog">
+                <div className="img1_blog">
+                     <img src="../public/placeholder.webp" alt="" />            
+                </div>
+                  <h2>Ecovaso: Desenvolvendo Bioplástico a base de extratos orgânicos</h2>
+                  <p>Indústria Sesi Patrocina um novo biopolímero no mercado</p>
+                  <button> Ver mais </button>
+             </section>
+             <section className="Trecho2_blog">
+                <div className="img2_blog">
+                     <img src="../public/placeholder.webp" alt="" />            
+                </div>
+                  <h2>Ecovaso: Desenvolvendo Bioplástico a base de extratos orgânicos</h2>
+                  <p>Indústria Sesi Patrocina um novo biopolimero no mercado</p>
+                  <button> Ver mais </button>
+             </section>
+             <section className="Trecho3_blog">
+                <div className="img3_blog">
+                     <img src="../public/placeholder.webp" alt="" />            
+                </div>
+                  <h2>Ecovaso: Desenvolvendo Bioplástico a base de extratos orgânicos</h2>
+                  <p>Indústria Sesi Patrocina um novo biopolimero no mercado</p>
+                  <button> Ver mais </button>
+             </section>
+        </div>
+        </>
     )
-}
+  }
