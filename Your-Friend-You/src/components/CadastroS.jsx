c4c948c6a5126a352a47458e5b0f00529560ed76
joão pedro silva <130461387+jprime617@users.noreply.github.com>
2024-05-27 12:07:20 -0300

    cadastro e login

--- Your-Friend-You/src/components/CadastroS.jsx
+++ Your-Friend-You/src/components/CadastroS.jsx
@@ -2,22 +2,24 @@ export default function CadastroS(){
     return(
         <section className="container-cadastro">
         <div className="cadastro-div">
-            <h1>Login</h1>
+            <h1>Cadastrar</h1>
             <div className="cadastro-div2">
+                <label>Nome:</label>
+                <input type="text" />
+                <label>Sobrenome:</label>
+                <input type="text" />
+                <label>Email:</label>
+                <input type="text" />
+                <label>Numero:</label>
+                <input type="number" />
                 <label>Usuário:</label>
                 <input type="text" name="" id="usuId" />
                 <label>Senha:</label>
                 <input type="password" name="" id="senId" />
-                <button onClick={Salvar}>Login</button>
-                <a href="/Cadastro">Criar Conta</a>
+                <button>Cadastrar</button>
             </div>
         </div>
         </section>
     )
 }
 
-function Salvar(){
-    let user = document.getElementById("usuId").value
-    let pass = document.getElementById("senId").value
-    localStorage.setItem(user,pass)
-}
--- Your-Friend-You/src/screens/Cadastro.jsx
+++ Your-Friend-You/src/screens/Cadastro.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import '../style.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import LoginS from '../components/LoginS'
+import CadastroS from '../components/CadastroS'
 
 
 function Cadastro() {
   return (
     <main>
         <Header />
-        <LoginS />
+        <CadastroS />
     </main>
   )
 }
